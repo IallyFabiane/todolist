7755e6a5809812eeb99a60692bdc22c113885d6b
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-06-17 15:00:14 -0300

    rota work

--- app.js
+++ app.js
@@ -9,29 +9,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 let newItems = [];
 let newItem = "";
+let workItems = [];
 
 app.get("/", function(req, res) {
     let today = new Date();
-    let currentDay = today.toLocaleDateString('pt-BR', { weekday: 'long' });
-    let hour = today.getHours();
-    let grettings ="";
+    let currentDay = today.toLocaleDateString('pt-BR', { weekday: 'long', day: 'numeric', month: 'long' }).toUpperCase();
   
-    if (hour > 12 && hour <= 18) {
-       grettings = "Boa tarde";
-    } else if (hour < 12 && hour <= 1) {
-        grettings = "Bom dia";
-    } else {
-        grettings= "Boa noite";
-    }
-    res.render("list", {kindOfDay: currentDay, salute: grettings, newListItems: newItems});
+    res.render("list", {listTitle: currentDay, newListItems: newItems});
 });
 
-
 app.post("/", function(req, res) {
+
     newItem = req.body.newItem;
-    newItems.push(newItem);
-    res.redirect("/");
-})
+
+    if (req.body.list === "Work") {
+        workItems.push(newItem);
+        res.redirect("/work");
+    } else {
+        newItems.push(newItem);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res) {
+    res.render("list",{listTitle: "Work List", newListItems: workItems} )
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
